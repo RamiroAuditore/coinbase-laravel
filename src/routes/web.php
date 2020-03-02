523c3387d8e921927a951607106689df20f12283
Ramiro Garcilazo <ramiroauditore@hotmail.com>
2020-03-02 16:34:35 -0600

    Test if library is properly loaded

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+    use CoinbaseCommerce\ApiClient;
     // MyVendor\contactform\src\routes\web.php
     Route::get('contact', function(){
         return 'Hello from the contact form package';
