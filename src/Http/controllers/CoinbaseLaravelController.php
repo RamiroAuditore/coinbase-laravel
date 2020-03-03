d5f399e4244508888ff3f3f1205e0989561d162d
Ramiro Garcilazo <ramiroauditore@hotmail.com>
2020-03-03 16:59:52 -0600

    create_charge completed

--- src/Http/controllers/CoinbaseLaravelController.php
+++ src/Http/controllers/CoinbaseLaravelController.php
@@ -17,11 +17,6 @@
 
         public function create_charge(Request $request)
         {
-            // $test_response = new \stdClass();
-            // $test_response->status = "Pending";
-            // $test_response_json = json_encode($test_response);
-            // CoinbaseLaravel::create(array_merge($request->all(), ['transaction_response' => $test_response_json]));
-            // return redirect('/');tkm
             ApiClient::init(env("COINBASE_API_KEY"));
             $chargeData = [
                 'name' => 'Compra en XOYcoin',
@@ -43,32 +38,6 @@
 
             $last_timeline_entry = count($charge->timeline) - 1;
             CoinbaseLaravel::create(['transaction_response' => json_encode($charge), 'order_code' => $charge->code, 'order_id' => $charge->id, 'status' => $charge->timeline[$last_timeline_entry]['status'], 'user_id' => $request->user_id, 'amount' => $request->amount, 'currency' => $request->currency]);
-
-            // foreach ($charge->addresses as $key => $value) {
-            //     // $value = $value * 2;
-            //     switch ($request->currency) {
-            //         case "bitcoincash":
-            //             $qr_string = $value;
-            //             break;
-            //         case "litecoin":
-            //             $qr_string = $value;
-            //             break;
-            //         case "bitcoin":
-            //             $qr_string = $value;
-            //             break;
-            //         case "ethereum":
-            //             $qr_string = $value;
-            //             break;
-            //         case "ethereum":
-            //             $qr_string = $value;
-            //             break;
-            //         case "ethereum":
-            //             $qr_string = $value;
-            //             break;
-            //         default:
-            //             $qr_string = "error";
-            //     }
-            // }
 
             return response()->json([
                 'pricing' => $charge->pricing,
