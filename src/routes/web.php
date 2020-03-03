425d2a0cd45dc93b2fc03d7ed46cedc8723c357f
Ramiro Garcilazo <ramiroauditore@hotmail.com>
2020-03-03 09:08:08 -0600

    Further testing of routing controller

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-    Route::group(['namespace' => 'coinbaselaravel\Http\Controllers', 'middleware' => ['web']], function(){
+    Route::group(['namespace' => 'CoinbaseLaravel\Http\Controllers', 'middleware' => ['web']], function(){
         Route::get('create_charge', 'CoinbaseLaravelController@create_charge');
         // Route::post('contact', 'ContactFormController@sendMail')->name('contact');
     });
