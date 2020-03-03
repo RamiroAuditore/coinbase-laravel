80848dc0cdd09e3d80128390bf48e9bcaa288e21
Ramiro Garcilazo <ramiroauditore@hotmail.com>
2020-03-03 09:37:00 -0600

    add name to post route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,5 @@
 <?php
     Route::group(['namespace' => 'coinbaselaravel\Http\Controllers', 'middleware' => ['web']], function(){
-        Route::post('create_charge', 'CoinbaseLaravelController@create_charge');
+        Route::post('create_charge', 'CoinbaseLaravelController@create_charge')->name('create_charge');
     });
 ?>
