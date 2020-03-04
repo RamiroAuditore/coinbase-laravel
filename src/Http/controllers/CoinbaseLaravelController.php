04c9876e146707abb6b4e357d5e105f3bba44dbf
Ramiro Garcilazo <ramiroauditore@hotmail.com>
2020-03-04 09:57:46 -0600

    Coinbase

--- src/Http/controllers/CoinbaseLaravelController.php
+++ src/Http/controllers/CoinbaseLaravelController.php
@@ -47,7 +47,7 @@
 
         public function charge_update(Request $request)
         {
-            return var_dump($request->event);
+            return var_dump($request->event["type"]);
         }
 
 
