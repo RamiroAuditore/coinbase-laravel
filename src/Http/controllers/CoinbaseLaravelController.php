fcd04199e18ca800b57a932dab1b02e3f48e0152
Ramiro Garcilazo <ramiroauditore@hotmail.com>
2020-03-04 10:43:00 -0600

    Attempt update table on webhook

--- src/Http/controllers/CoinbaseLaravelController.php
+++ src/Http/controllers/CoinbaseLaravelController.php
@@ -3,6 +3,7 @@
     namespace coinbaselaravel\Http\Controllers;
     use App\Http\Controllers\Controller;
     use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\DB;
     use coinbaselaravel\Models\CoinbaseLaravel;
     use CoinbaseCommerce\ApiClient;
     use CoinbaseCommerce\Resources\Charge;
@@ -48,7 +49,12 @@
         public function charge_update(Request $request)
         {
             $last_timeline_entry = count($request->event["data"]["timeline"]) - 1;
-            return $request->event["data"]["timeline"][$last_timeline_entry];
+            $charge_to_update = DB::table('coinbase_transactions')->where('order_code', $request->event["data"]["code"])->first();
+            $updated_charge = DB::table('coinbase_transactions')
+            ->where('id', $charge_to_update->id)
+            ->update(['status' => $request->event["data"]["timeline"][$last_timeline_entry]['status']]);
+
+            return $updated_charge;
         }
 
 
