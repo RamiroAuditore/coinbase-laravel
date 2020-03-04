cf58edd2b78c5d96466130de7cc27022e557e3f6
Ramiro Garcilazo <ramiroauditore@hotmail.com>
2020-03-04 11:14:14 -0600

    add token amount to insert

--- src/Http/controllers/CoinbaseLaravelController.php
+++ src/Http/controllers/CoinbaseLaravelController.php
@@ -38,7 +38,7 @@
             }
 
             $last_timeline_entry = count($charge->timeline) - 1;
-            CoinbaseLaravel::create(['transaction_response' => json_encode($charge), 'order_code' => $charge->code, 'order_id' => $charge->id, 'status' => $charge->timeline[$last_timeline_entry]['status'], 'user_id' => $request->user_id, 'amount' => $request->amount, 'currency' => $request->currency]);
+            CoinbaseLaravel::create(['transaction_response' => json_encode($charge), 'order_code' => $charge->code, 'order_id' => $charge->id, 'status' => $charge->timeline[$last_timeline_entry]['status'], 'user_id' => $request->user_id, 'amount' => $request->amount, 'currency' => $request->currency, 'token_amount' => $request->token_amount]);
 
             return response()->json([
                 'pricing' => $charge->pricing,
